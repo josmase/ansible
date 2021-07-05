9fb1ace9eb8cda877000b29e0216c22679f6442c
Jonas Lundberg <lundbergjonas96@gmail.com>
2021-07-05 20:37:20 +0200

    Fix permission problems with grafana with maintenance script

--- roles/docker/templates/scripts/maintenance.sh
+++ roles/docker/templates/scripts/maintenance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 backup_path={{ storage_dir }}/backups
-appdat_path={{ appdata_path }}
+appdata_path={{ appdata_path }}
 script_dir={{ script_dir }}
 downloads_dir={{ storage_dir }}/downloads
 
@@ -8,10 +8,14 @@ echo =====================  $(date)  ===========================================
 source "${script_dir}/update.sh"
 source "${script_dir}/down.sh"
 
-rsync -aP --delete "${appdat_path}" "${backup_path}"
-#snapraid -c ../snapraid.conf sync
+rsync -aP --delete "${appdata_path}" "${backup_path}"
+
+#Grafana requires UID and GUID 472 since the container doesn't support setting them. This has to be done before starting it.
+chown -R 472:472 "${appdata_path}/grafana"
 
 source "${script_dir}/start.sh"
 
+#These are to fix permission errors caused by restarting somehting that is writing to downloads. Not sure what.
 chown -R {{ main_username }}:{{ main_groupname }} "${downloads_dir}"
 chmod -R 755 "${downloads_dir}"
+
