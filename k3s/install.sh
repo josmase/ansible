6c871ce475b07aed0832543ac9d21f180d4e42e6
Jonas Lundberg <lundbergjonas96@gmail.com>
2022-11-27 17:19:37 +0000

    Move helm install out of racher install

---
+++ k3s/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Exit on error
+set -e
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
--- k3s/rancher/install.sh
+++ k3s/rancher/install.sh
@@ -2,9 +2,11 @@
 #Exit on error
 set -e
 
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
-
-
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable && helm repo update
 kubectl apply -f ./namespace.yaml
 helm install rancher rancher-stable/rancher --namespace cattle-system -f values.yaml
+kubectl -n cattle-system rollout status deploy/rancher
+
+
+#Reset password
+#kubectl -n cattle-system exec $(kubectl -n cattle-system get pods -l app=rancher | grep '1/1' | head -1 | awk '{ print $1 }') -- reset-password
