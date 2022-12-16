afa70886266bbccc56b7301f05a0c03f67304bcf
Jonas Lundberg <lundbergjonas96@gmail.com>
2022-12-16 21:45:57 +0000

    Re-enable permission fix as it's still an issue

--- ansible/roles/docker/templates/scripts/maintenance.sh
+++ ansible/roles/docker/templates/scripts/maintenance.sh
@@ -16,13 +16,13 @@ tar -cvpzf "${BACKUP_PATH}/${FILENAME}" "${APPDATA_PATH}"
 
 
 #Grafana requires UID and GUID 472 since the container doesn't support setting them. This has to be done before starting it.
-chown -R 472:472 "${APPDATA_PATH}/grafana"
+#chown -R 472:472 "${APPDATA_PATH}/grafana"
 
 source "${SCRIPT_DIR}/start.sh"
 
 #These are to fix permission errors caused by restarting somehting that is writing to downloads. Not sure what.
-#chown -R {{ main_username }}:{{ main_groupname }} "${DOWNLOADS_DIR}"
-#chmod -R 755 "${DOWNLOADS_DIR}"
+chown -R {{ main_username }}:{{ main_groupname }} "${DOWNLOADS_DIR}"
+chmod -R 755 "${DOWNLOADS_DIR}"
 
 #There is an issue with EasyAudioEncoder and removing all codecs and restarting solves it for a while.
 rm -r "${APPDATA_PATH}/plex/Library/Application Support/Plex Media Server/Codecs/*" && docker restart plex
