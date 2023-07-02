23343b1356644913837eef2dc23623322da46e07
Jonas Lundberg <lundbergjonas96@gmail.com>
2023-07-02 17:56:34 +0000

    Add vms and change defaults

--- terraform/constants.tf
+++ terraform/constants.tf
@@ -5,7 +5,6 @@ locals {
     target_node         = var.proxmox_node
     qemu_os             = "other"
     os_type             = "cloud-init"
-    agent               = 1
     full_clone          = true
     template            = var.ubuntu_template
     cores               = 2
@@ -15,7 +14,7 @@ locals {
     gateway             = "${local.subnet}.1"
     ssh_user            = "ubuntu"
     public_ssh_key      = var.public_ssh_key
-    disk_type           = "virtio"
+    disk_type           = "scsi"
     storage_dev         = var.proxmox_storage
     network_bridge_type = "vmbr0"
     network_model       = "virtio"
@@ -33,6 +32,7 @@ locals {
   kubernetes_node = merge(local.base_machine, {
     description = "Kubernetes node"
     cores       = 2
+    storage = "300G"
   })
 
   machine_map = {
@@ -46,6 +46,20 @@ locals {
         id         = machine_id
         name       = "node-${machine_id}"
         ip_address = "${local.subnet}.${machine_id}"
+      }) },
+      { 105 = merge(local.base_machine, {
+        id          = "105"
+        name        = "media-server"
+        storage     = "150G"
+        memory      = 8192
+        description = "Main media server for stuff that is not yet running in kubernetes"
+        ip_address  = "${local.subnet}.105"
+      }) },
+      { 110 = merge(local.base_machine, {
+        id          = "110"
+        name        = "utils"
+        description = "For running utils that should always be running, for example vpn. So that all other VMs can safely be restarted without risk of being locked out."
+        ip_address  = "${local.subnet}.110"
       }) }
     )
   }
--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -1,21 +1,48 @@
 variable "proxmox_host" {
-    type=string
+  type = string
 }
 
 variable "proxmox_id" {
   description = "Token id for the proxmox user"
   type        = string
-  sensitive = true
+  sensitive   = true
 }
 
 variable "proxmox_secret" {
   description = "Token secret for the proxmox user"
   type        = string
-  sensitive = true
+  sensitive   = true
 }
 
 variable "virtual_machines" {
-  type        = map
+  type = map(object({
+    id                  = string
+    name                = string
+    qemu_os             = string
+    description         = string
+    target_node         = string
+    os_type             = string
+    full_clone          = string
+    template            = string
+    memory              = number
+    socket              = number
+    cores               = number
+    ssh_user            = string
+    public_ssh_key      = string
+    ssh_user            = string
+    ip_address          = string
+    gateway             = string
+    cloud_init_pass     = string
+    automatic_reboot    = bool
+    dns_servers         = string
+    storage_dev         = string
+    disk_type           = string
+    storage             = string
+    network_bridge_type = string
+    network_model       = string
+    network_firewall    = bool
+
+  }))
   default     = {}
   description = "Identifies the object of virtual machines."
 }
