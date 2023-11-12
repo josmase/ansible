0a5f8f87933619de6e3662450f35069d76785609
Jonas Lundberg <lundbergjonas96@gmail.com>
2023-11-12 22:52:09 +0000

    Add dns managemnet using cloudflare

---
+++ terraform/dns/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
+
+resource "cloudflare_record" "hejsan" {
+  zone_id = var.zone_id
+  name    = var.domain
+  type    = "A"
+  value   = var.external_ip
+  proxied = true
+}
+
+resource "cloudflare_record" "local" {
+  for_each = {
+    "*.local"         = "192.168.0.181"
+    "media.local"     = "192.168.0.105"
+    "proxmox.local"   = "192.168.0.100"
+    "storage.local"   = "192.168.0.102"
+    "*.staging.local" = "192.168.0.182"
+  }
+  zone_id = var.zone_id
+  name    = each.key
+  type    = "A"
+  value   = each.value
+  proxied = false
+}
+
+# Proxied CNAME Records
+resource "cloudflare_record" "proxied_cnames" {
+  for_each = toset(["api", "assistant", "budget", "dohi", "emby", "fest", "it-tools", "jellyfin", "ombi", "plex", "serble", "wireguard"])
+  zone_id  = var.zone_id
+  name     = each.key
+  type     = "CNAME"
+  value    = "hejsan.xyz"
+  proxied  = true
+}
+
+# Non-Proxied CNAME Record
+resource "cloudflare_record" "non_proxied_cname" {
+  zone_id = var.zone_id
+  name    = "wireguard"
+  type    = "CNAME"
+  value   = "hejsan.xyz"
+  proxied = false
+}
---
+++ terraform/dns/variables.tf
@@ -0,0 +1,19 @@
+variable "api_token" {
+  description = "Cloudflare API token"
+}
+
+variable "zone_id" {
+  description = "Cloudflare Zone ID"
+}
+
+variable "account_id" {
+    description = "Cloudflare account ID for zone" 
+}
+
+variable "domain" {
+  description = "Domain name" 
+}
+
+variable "external_ip" {
+  description = "External IP address"
+}
