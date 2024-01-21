e4ab3c39d406fdc0010d48ce97b11c7397213a4d
Jonas Lundberg <lundbergjonas96@gmail.com>
2024-01-21 19:11:04 +0000

    k3s has moved to josmase/flux

--- k3s/vault/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Exit on error
-set -e
-
-helm repo add hashicorp https://helm.releases.hashicorp.com && helm repo update 
-helm upgrade --install \
-  --values=values.yaml \
-  --wait vault hashicorp/vault 
