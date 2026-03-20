4057fce12e470716f57d5f46ca302dd8600c5821
Jonas Lundberg <lundbergjonas96@gmail.com>
2026-03-20 17:31:04 +0100

    Use Wolf's compositor instead of starting own Xwayland

--- ansible/roles/services/game_streaming_wolf/files/lutris-desktop/scripts/launch-comp.sh
+++ ansible/roles/services/game_streaming_wolf/files/lutris-desktop/scripts/launch-comp.sh
@@ -13,25 +13,31 @@ function launcher() {
     chmod 755 ~/Desktop ~/Documents ~/Downloads ~/Games ~/Pictures
   fi
 
-  export DESKTOP_SESSION=openbox
+  # Set up environment to use Wolf's compositor
   export XDG_CURRENT_DESKTOP=openbox
-  export XDG_SESSION_TYPE="x11"
+  export XDG_SESSION_TYPE=wayland
   export _JAVA_AWT_WM_NONREPARENTING=1
-  export GDK_BACKEND=x11
-  export MOZ_ENABLE_WAYLAND=0
-  export QT_QPA_PLATFORM="xcb"
+  export GDK_BACKEND=wayland
+  export MOZ_ENABLE_WAYLAND=1
+  export QT_QPA_PLATFORM=wayland
   export QT_AUTO_SCREEN_SCALE_FACTOR=1
   export QT_ENABLE_HIGHDPI_SCALING=1
-  export DISPLAY=:0
   export GTK_THEME=Arc-Dark:dark
+  
+  # Use the wayland display provided by Wolf
+  export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
+  export DISPLAY=""
 
-  export REAL_WAYLAND_DISPLAY=$WAYLAND_DISPLAY
-  unset WAYLAND_DISPLAY
+  gow_log "[Openbox] - Starting desktop environment (connecting to compositor)"
 
-  gow_log "[Openbox] - Starting desktop environment"
-
-  # Start Xwayland + Openbox + tint2 with dbus-run-session
-  dbus-run-session -- bash -E -c "WAYLAND_DISPLAY=\$REAL_WAYLAND_DISPLAY Xwayland :0 & sleep 2 && openbox-session & sleep 1 && tint2 & pcmanfm --desktop & wait"
+  # Start Openbox + tint2 + pcmanfm using dbus-run-session
+  dbus-run-session -- bash -E -c "
+    openbox &
+    sleep 2
+    tint2 &
+    pcmanfm --desktop &
+    wait
+  "
 }
 
 # Run launcher if this script is executed directly
